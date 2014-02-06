5a8791cde05a0fcfd46a663883d59033da0edf0f
luis alberto mayta <slovacus@gmail.com>
2014-02-06 06:18:22 -0500

    add Elements LammCore

--- tests/LammCore/Framework/TestCase.php
+++ tests/LammCore/Framework/TestCase.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-namespace CoreTest\Framework;
+namespace LammCore\Test\Framework;
 
 use PHPUnit_Framework_TestCase;
 
@@ -29,5 +21,3 @@ class TestCase extends PHPUnit_Framework_TestCase
     }
 
 }
-
-
